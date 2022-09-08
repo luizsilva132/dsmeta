0ab3721b6c32601eb6e33185cd12465e57f5b21c
Luiz Henrique Rosa Da Silva <rik.killer@hotmail.com>
2022-09-08 20:25:34 -0300

    Twilio SMS

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleCrontoller.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleCrontoller.java
@@ -8,12 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.services.SaleService;
+import com.devsuperior.dsmeta.services.SmsService;
 
 
 @RestController
@@ -21,7 +23,10 @@ import com.devsuperior.dsmeta.services.SaleService;
 public class SaleCrontoller {
     
 	@Autowired
-	public SaleService service;
+	private SaleService service;
+	
+	@Autowired
+	private SmsService smsService;
 	
 	@GetMapping
 	public Page<Sale> findSales(
@@ -31,4 +36,10 @@ public class SaleCrontoller {
 		return service.findSales(minDate, maxDate, pageable);
 		
 	}
+	@GetMapping("/{id}/notification")
+	public void notifySms(@PathVariable Long id) {
+		smsService.sendSms(id);
+	}
+	
+	
 }
